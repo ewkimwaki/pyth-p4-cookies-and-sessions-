3c8b6a9c15a1602143b6d1b2aeee387c3d21c907
ewkimwaki <e.w.kimwaki@gmail.com>
2024-04-24 11:02:06 +0300

    first commit

---
+++ server/app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, jsonify, session
+from flask_migrate import Migrate
+
+from models import db, Article, User
+
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/clear')
+def clear_session():
+    session['page_views'] = 0
+    return {'message': '200: Successfully cleared session data.'}, 200
+
+@app.route('/articles')
+def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
+
+
+
+@app.route('/articles/<int:id>')
+def show_article(id):
+    session['page_views'] = session.get('page_views', 0)
+    session['page_views'] += 1
+    
+    article = Article.query.filter_by(id=id).first()
+    article_dict = article.to_dict()
+    
+    if session['page_views'] <= 3:
+        return jsonify(article_dict), 200
+    else:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
+    
+if __name__ == '__main__':
+    app.run(port=5555)
